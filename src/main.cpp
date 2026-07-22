ca236eb502d5cd1a771484a26ff9596b29b6d9fb
Daniil Gorbunov <dagoroff@gmail.com>
2026-07-23 01:09:58 +0300

    primary classes

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include "i_network.hpp"
+#include "tui.hpp"
+#include "node.hpp"
+
+int main (int argc, char *argv[]) {
+	// start
+	// init tui
+	// ask tui for comand
+	// switch by command
+	// ask i_network to do work (&ostream - for printing)
+	// send i_network answer to tui
+	return 0;
+}
